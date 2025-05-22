d95bb28f9877883289a000e1c2450b157a6299c3
Hanzala <bigapplestorekmr@gmail.com>
2025-05-22 12:33:29 +0530

    header responsive

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <div className="flex space-x-30 m-10 justify-around items-center">
-        <h1 className="text-blue-900 text-3xl font-bold font-mono"> Todo App </h1>
-        <button className="border border-solid border-blue-900 px-5 font-bold py-2 rounded-xl bg-blue-200 "> Add Todo </button>
+      <div className="flex md:space-x-30 md:m-10 my-10 mx-2  justify-between md:justify-around items-center">
+        <h1 className="text-blue-900 md:text-3xl text-xl font-bold font-mono"> Todo App </h1>
+        <button className="border border-solid border-blue-900 px-3 md:px-5 font-roboto font-bold md:py-2 rounded-xl bg-blue-200 "> Add Todo </button>
       </div>
     </div>
   );
